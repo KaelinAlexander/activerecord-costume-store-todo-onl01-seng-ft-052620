4647c8864a7c707d66e03c3f2564968dad919af0
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 17:29:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,11 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :costumes
-      t.integer :number_of_employees
-      t.boolean :in_business
-      t.datetime :opening
-      t.datetime :closing
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 
