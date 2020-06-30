ac470d80dc063731914495885e45211bd12ad3e1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 17:27:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
 
   def change
     create_table :costume_stores do |t|
-
+      t.string :name
+      t.string :location
+      t.integer :costumes
+      t.integer :number_of_employees
     end
   end
 
