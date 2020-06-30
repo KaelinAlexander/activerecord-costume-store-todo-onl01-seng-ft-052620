93f4e834c3cd09fed4b47c4d2790f5edfeefb9ba
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 17:26:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,10 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costume_stores do |t|
+
+    end
+  end
+
+end
