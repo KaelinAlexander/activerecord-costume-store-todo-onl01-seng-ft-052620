d99a29cc5fd59ab96f6d541a64baa3ea7c08528b
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 17:33:43 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,13 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
 
-def change
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
 
-end
+  end
 
 end
